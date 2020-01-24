078ffe2dddee8a97e559b32da27844041b1d224f
Daniel <daniel@alvestechnology.io>
2020-01-24 13:43:24 +0000

    add UCS function

--- f5_backup.py
+++ f5_backup.py
@@ -4,18 +4,22 @@ from keyvault import GetSecret
 class F5():
     """This class is used to interact with an F5 appliance"""
 
-    def __init__(self, password):
-        """This function retrieves a secret from an Azure Key Vault"""
+    def __init__(self, username, password):
+        """This function creates a UCS archive on an F5 and then uploads it to blob storage"""
+        self.username = username
         self.password = password
+
         # Connect to the BigIP
-        bigip = ManagementRoot("localhost", "admin", self.password, port=5556, verify=False)
+        bigip = ManagementRoot("localhost", self.username, self.password, port=5556, verify=False)
 
-        # Get a list of all pools on the BigIP and print their name and their
-        # members' name
-        pools = bigip.tm.ltm.pools.get_collection()
-        for pool in pools:
-            for member in pool.members_s.get_collection():
-                print(member.name)
+        # Get a list of all the UCS files on the F5s local storage
+        ucs = bigip.tm.sys.ucs.load()
+        items = ucs.items
+        for item in items:
+            print(item["apiRawValues"]["filename"])
 
-f5_password = GetSecret("reformMgmtF5Internal-password").secret_value
-F5(f5_password)
+f5_username = GetSecret("tactical-f5-user").secret_value
+f5_password = GetSecret("tactical-f5-pw").secret_value
+F5(f5_username, f5_password)
+
+#mgmt.shared.file_transfer.ucs_downloads.download_file('config.ucs', '/Users/citizenelah/Downloads/config.ucs')
